62b87379c800d914bbc4724691eea6534fadebe8
Abhijithbiju2002 <abhiju7@gmail.com>
2025-01-22 11:33:41 +0530

    commit 1
    added player ctrl script

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -17,13 +17,13 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 scale = transform.localScale;
 
-        if (speed < 0f)
+        if (speed < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
         }
-        else if (speed > 0f)
+        else if (speed > 0)
         {
-            scale.x *= Mathf.Abs(scale.x);
+            scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
 
