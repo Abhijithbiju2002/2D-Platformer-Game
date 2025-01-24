fd1d04af38f18c3473a3b91fda08195ede3b3e2d
Abhijithbiju2002 <abhiju7@gmail.com>
2025-01-24 16:04:47 +0530

    commit f4 5
    death and reload scene

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -125,12 +125,6 @@ public class PlayerMovement : MonoBehaviour
             isGrounded = false;
         }
     }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.transform.tag == "GameOver")
-        {
-            Destroy(gameObject);
-        }
-    }
+
 
 }
